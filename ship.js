b02b1c1f5672f7214455737980df587b42318e43
polymorphicus <120289616+polymorphicus@users.noreply.github.com>
2023-03-07 19:07:44 -0500

    Finish ship factory function

--- ship.js
+++ ship.js
@@ -1,8 +1,39 @@
-const Ship = (length) =>  {
-    const shipLength = length;
+const Ship = (shipArray) =>  {
+    const shipLength = shipArray.length;
 
-    return { length: shipLength }
+    const hit = (indexNumber) => {
+        if (indexNumber > shipLength) {return;}
+
+        shipArray[indexNumber] = 'hit'
+    }
+
+    const returnArray = () => {
+        return shipArray;
+    }
+
+    const isSunk = () => {
+        let hits = 0;
+        let sunk = false;
+        for (let i = 0; i < shipLength; i++) {
+            if (shipArray[i] === 'hit') {
+                hits++;
+            }
+        }
+
+        if (hits === shipLength) {
+            sunk = true;
+        }
+
+        return sunk;
+        
+    }
+
+    return { length: shipLength, hit, returnArray, isSunk}
 }
+
+const newShip = Ship(['hit', 'hit', 'hit', 'hit', 'E1'])
+
+console.log(newShip.isSunk())
 
 
 module.exports = Ship
--- ship.test.js
+++ ship.test.js
@@ -1,5 +1,50 @@
 const Ship = require('./ship')
 
-it('return the ship length', () => {
-    expect(Ship(5)).toStrictEqual({ length: 5 })
+
+it('only hit parts of the array', () => {
+    const ship1 = Ship(['A1','B1','C1','D1'])
+    ship1.hit(5);
+    expect(ship1.returnArray()).toStrictEqual(['A1','B1','C1','D1'])
 })
+
+it('ship is hit', () => {
+    const ship2 = Ship(['A1','B1','C1','D1'])
+    ship2.hit(1);
+    expect(ship2.returnArray()).toStrictEqual(['A1','hit','C1','D1'])
+})
+
+it('ship is hit many times', () => {
+    const ship3 = Ship(['A1','B1','C1','D1'])
+    ship3.hit(0);
+    ship3.hit(1);
+    ship3.hit(2);
+    ship3.hit(3);
+    expect(ship3.returnArray()).toStrictEqual(['hit','hit','hit','hit'])
+})
+
+it('ship is not sunk (1)', () => {
+    const ship4 = Ship(['A1','B1','C1','D1'])
+    
+    expect(ship4.isSunk()).toBe(false)
+})
+
+it('ship is not sunk (2)', () => {
+    const ship5 = Ship(['A1','B1','C1','D1'])
+
+    ship5.hit(0);
+    ship5.hit(2);
+
+    expect(ship5.isSunk()).toBe(false)
+})
+
+it('ship is sunk', () => {
+    const ship6 = Ship(['A1','B1','C1','D1'])
+
+    ship6.hit(0);
+    ship6.hit(1);
+    ship6.hit(2);
+    ship6.hit(3);
+
+    expect(ship6.isSunk()).toBe(true)
+})
+
